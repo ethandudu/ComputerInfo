1cb86764cc944c1f5497aea05641ab961c67c8e7
Ethan <41381482+ethandudu@users.noreply.github.com>
2025-02-08 14:33:07 +0100

    adds os infos

--- ComputerInfo/LibSvc/SvcOs.cs
+++ ComputerInfo/LibSvc/SvcOs.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -34,7 +35,7 @@ namespace ComputerInfo.LibSvc
 
         public override string GetTextInfo()
         {
-            throw new NotImplementedException();
+            return string.Format("OS: {0} - {1} - {2} - {3} - {4}", m_oOs.Name, m_oOs.BuildVersion, m_oOs.UserLogin, m_oOs.ComputerName, m_oOs.Architecture);
         }
 
         public override System.Windows.Forms.TreeNode GetTreeNode()
@@ -44,7 +45,38 @@ namespace ComputerInfo.LibSvc
 
         public static OS GetOs()
         {
-            throw new NotImplementedException();
+            OS oOs = new OS();
+            oOs.Name = GetOSFullName();
+            oOs.BuildVersion = System.Environment.OSVersion.Version.ToString();
+            oOs.UserLogin = System.Environment.UserName;
+            oOs.ComputerName = System.Environment.MachineName;
+            oOs.Architecture = GetArchitecture();
+            return oOs;
+        }
+
+        private static Architecture GetArchitecture()
+        {
+            switch (System.Environment.Is64BitOperatingSystem)
+            {
+                case true:
+                    return Architecture.x64;
+                case false:
+                    return Architecture.x86;
+                default:
+                    return Architecture.UNKNOWN;
+            }
+        }
+
+        private static string GetOSFullName()
+        {
+            string osFullName = string.Empty;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT Caption FROM Win32_OperatingSystem");
+            foreach (ManagementObject os in searcher.Get())
+            {
+                osFullName = os["Caption"].ToString();
+                break;
+            }
+            return osFullName;
         }
     }
 
--- ComputerInfo/Program.cs
+++ ComputerInfo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComputerInfo.LibSvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -17,6 +18,8 @@ namespace ComputerInfo
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
+            SvcOs oSvcOS = new SvcOs();
+            Console.WriteLine(oSvcOS.GetTextInfo());
         }
     }
 }
