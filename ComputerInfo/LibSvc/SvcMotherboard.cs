fe098d643d9c6a8d27ea53958217a583ddd6f967
Ethan <41381482+ethandudu@users.noreply.github.com>
2025-02-08 15:55:35 +0100

    Adds SvcMotherboard support

--- ComputerInfo/LibSvc/SvcMotherboard.cs
+++ ComputerInfo/LibSvc/SvcMotherboard.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ComputerInfo.LibSvc
 {
@@ -25,11 +27,40 @@ namespace ComputerInfo.LibSvc
 
         public override System.Xml.XmlElement GetXmlElement()
         {
-            throw new NotImplementedException();
+            XmlDocument oXmlDocument = new XmlDocument();
+            XmlElement oXmlElement = oXmlDocument.CreateElement("Motherboard");
+            if (!string.IsNullOrEmpty(m_oMotherboard.Name))
+            {
+                XmlElement oNameElement = oXmlDocument.CreateElement("Name");
+                oNameElement.InnerText = m_oMotherboard.Name;
+                oXmlElement.AppendChild(oNameElement);
+            }
+            if (!string.IsNullOrEmpty(m_oMotherboard.Vendor))
+            {
+                XmlElement oVendorElement = oXmlDocument.CreateElement("Vendor");
+                oVendorElement.InnerText = m_oMotherboard.Vendor;
+                oXmlElement.AppendChild(oVendorElement);
+            }
+            if (!string.IsNullOrEmpty(m_oMotherboard.Serial))
+            {
+                XmlElement oSerialElement = oXmlDocument.CreateElement("Serial");
+                oSerialElement.InnerText = m_oMotherboard.Serial;
+                oXmlElement.AppendChild(oSerialElement);
+            }
+            if (!string.IsNullOrEmpty(m_oMotherboard.BiosVersion))
+            {
+                XmlElement oBiosVersionElement = oXmlDocument.CreateElement("BiosVersion");
+                oBiosVersionElement.InnerText = m_oMotherboard.BiosVersion;
+                oXmlElement.AppendChild(oBiosVersionElement);
+            }
+            return oXmlElement;
         }
         public override string GetTextInfo()
         {
-            throw new NotImplementedException();
+            string sTextInfo = "Motherboard Information: ";
+            sTextInfo += string.Format("Name: {0} - Manufacturer: {1} - Serial: {2} - Bios Version: {3}",
+                m_oMotherboard.Name, m_oMotherboard.Vendor, m_oMotherboard.Serial, m_oMotherboard.BiosVersion);
+            return sTextInfo;
         }
         public override System.Windows.Forms.TreeNode GetTreeNode()
         {
@@ -37,7 +68,16 @@ namespace ComputerInfo.LibSvc
         }
         public static Motherboard GetMotherboard()
         {
-            throw new NotImplementedException();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+            Motherboard oMotherboard = new Motherboard();
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                oMotherboard.Name = queryObj["Product"].ToString();
+                oMotherboard.Vendor = queryObj["Manufacturer"].ToString();
+                oMotherboard.Serial = queryObj["SerialNumber"].ToString();
+                oMotherboard.BiosVersion = queryObj["Version"].ToString();
+            }
+            return oMotherboard;
         }
 
         #endregion
