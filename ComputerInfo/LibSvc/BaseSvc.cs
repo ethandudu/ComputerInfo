7e9721e596311924de89929eaa5b5523218954d8
Ethandudu <41381482+ethandudu@users.noreply.github.com>
2025-02-08 14:12:14 +0100

    Adds base Service

---
+++ ComputerInfo/LibSvc/BaseSvc.cs
@@ -0,0 +1,55 @@
+﻿using ComputerInfo.LibSvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ComputerInfo.LibSvc
+{
+    enum SvcType
+    {
+        Disk,
+        Battery,
+        OS,
+        Network,
+        CPU,
+        RAM,
+        GPU,
+        Motherboard,
+        UNKNOWN
+    }
+
+    internal abstract class BaseSvc
+    {
+        protected SvcType m_eSvcType;
+
+        #region Constructor
+
+        public BaseSvc()
+        {
+            m_eSvcType = SvcType.UNKNOWN;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public SvcType SvcType
+        {
+            get { return m_eSvcType; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public abstract XmlElement GetXmlElement();
+        public abstract String GetTextInfo();
+        public abstract TreeNode GetTreeNode();
+
+        #endregion
+    }
+}
---
+++ ComputerInfo/LibSvc/SvcDisk.cs
@@ -0,0 +1,52 @@
+﻿using ComputerInfo.LibPcComponent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerInfo.LibSvc
+{
+    internal class SvcDisk : BaseSvc
+    {
+        private Disk[] m_aoDisks;
+
+        #region Constructor
+
+        public SvcDisk()
+        {
+            m_eSvcType = SvcType.Disk;
+            m_aoDisks = GetDisks();
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        public override System.Xml.XmlElement GetXmlElement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetTextInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override System.Windows.Forms.TreeNode GetTreeNode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Disk[] GetDisks()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
